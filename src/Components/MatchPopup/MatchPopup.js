4adc7c26cbc14133a707417f52634cb03d831488
Joseph Marella <Josephmarella97@gmail.com>
2021-04-20 19:56:35 -0400

    Added fireworks animation to match popup

--- src/Components/MatchPopup/MatchPopup.js
+++ src/Components/MatchPopup/MatchPopup.js
@@ -6,7 +6,12 @@ import { popupTransition, backdropTransition } from '../../Util/PageTransitions'
 export default function MatchPopup(props) {
     return (
         <div className='matchPopupPageWrapper'>
-            <motion.div key='matchPopupBackdrop' className='matchPopupBackdrop' initial={backdropTransition.initial} animate={backdropTransition.in} exit={backdropTransition.out} transition={{ duration: .25 }}/>
+            <motion.div key='matchPopupBackdrop' className='matchPopupBackdrop' initial={backdropTransition.initial} animate={backdropTransition.in} exit={backdropTransition.out} transition={{ duration: .25 }}>
+            <div class="pyro">
+                <div class="before"></div>
+                <div class="after"></div>
+            </div>
+            </motion.div>
             <motion.div key='matchPopupContainer' className='matchPopupContainer' initial={popupTransition.initial} animate={popupTransition.in} exit={popupTransition.out} transition={{ duration: .15 }}>
                 <img className='matchPopupImage' src={props.matchProfile.profilePicture}/>
                 <p className='matchPopupHeaderText'>You matched with {props.matchProfile.firstName} {props.matchProfile.lastName}!</p>
